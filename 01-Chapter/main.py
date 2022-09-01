ae0a79ec4a57475ba3d23f6aede8887619f76881
rikhi.ram <rikhi.ram@qovarian.com>
2022-09-01 11:59:00 +0530

    added logic to check number is even or odd

--- 01-Chapter/main.py
+++ 01-Chapter/main.py
@@ -2,5 +2,13 @@ def print_hi(name):
     print(f'Welcome to the python world, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def find_even_number(numbers):
+    for i in numbers:
+        if i % 2 == 0:
+            print(i)
+
+
+
 if __name__ == '__main__':
     print_hi('Maha')
+    find_even_number([1,3,4,6,90,7,34])
