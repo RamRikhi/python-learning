b5047152ab61ed44913c63e105d5eebc03e39256
Rikhi Ram Satnami <57804353+RamRikhi@users.noreply.github.com>
2022-09-02 19:38:50 +0530

    Feature/python basics (#7)
    * message updated to maha
    
    * added logic to check number is even or odd
    
    * added logic to check number is even or odd
    
    * code updated
    
    * code updated
    
    * code updated
    
    Co-authored-by: rikhi.ram <rikhi.ram@qovarian.com>

--- 01-Chapter/main.py
+++ 01-Chapter/main.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 
 def send_greeting(name):
-    current_time = datetime.now()
-    current_time = current_time.hour
+    current_time =  datetime.now().hour
     if current_time <= 12:
         print("Good morning " + name)
-        print(current_time)
     elif current_time <= 16:
         print("Good afternoon " + name)
     elif current_time <= 23:
