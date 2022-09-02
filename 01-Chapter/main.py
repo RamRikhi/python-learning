2c84352b69611dea57f34fc5bb35d6195f2e2e2a
rikhi.ram <rikhi.ram@qovarian.com>
2022-09-02 19:11:19 +0530

    code updated

--- 01-Chapter/main.py
+++ 01-Chapter/main.py
@@ -1,14 +1,5 @@
 def print_hi(name):
     print(f'Welcome to the python world, {name}')
 
-
-def find_even_number(numbers):
-    for i in numbers:
-        if i % 2 == 0:
-            print(i)
-
-
-
 if __name__ == '__main__':
-    print_hi('Maha')
-    find_even_number([1,3,4,6,90,7,34])
+    print_hi('Rikhi')
