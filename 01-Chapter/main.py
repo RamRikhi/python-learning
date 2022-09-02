a644c4bb9c126e139280311c094a81c43859be44
rikhi.ram <rikhi.ram@qovarian.com>
2022-09-02 19:35:44 +0530

    code updated

--- 01-Chapter/main.py
+++ 01-Chapter/main.py
@@ -1,5 +1,17 @@
-def print_hi(name):
-    print(f'Welcome to the python world, {name}')
+from datetime import datetime
+
+def send_greeting(name):
+    current_time = datetime.now()
+    current_time = current_time.hour
+    if current_time <= 12:
+        print("Good morning " + name)
+        print(current_time)
+    elif current_time <= 16:
+        print("Good afternoon " + name)
+    elif current_time <= 23:
+        print("Good evening " + name)
+    else:
+        print("Good night " + name)
 
 if __name__ == '__main__':
-    print_hi('Rikhi')
+    send_greeting('Rikhi')
