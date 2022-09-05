f7865f0bba25cc5af685237365f3006ec1edfbd2
Rikhi Ram Satnami <57804353+RamRikhi@users.noreply.github.com>
2022-09-05 18:42:22 +0530

    Feature/python basics (#20)
    * chapter-1
    
    * chapter-1-single line comment
    
    * multi line comments program
    
    * comment.py file is removed from main branch
    
    * python chapter 2 started
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * python learing is in progress
    
    * md file updated
    
    * chapter 2 started
    
    Co-authored-by: rikhi.ram <rikhi.ram@qovarian.com>

---
+++ 02-Chapter/greet_user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+# Author:  Rikhi
+# Date: 05/09/2022
+# Copyright @ 2022
+
+# This function will take name as input and based on current system time it will greet the user
+def greet_user(name):
+    date = datetime.now().hour
+    if date < 12:
+            print("Good Morning " + name)
+    elif date < 17:
+            print("Good Afternoon " + name)
+    elif date < 23:
+            print("Good Evening " + name)
+    else:
+            print("Good Night " + name)
+
+# Calling greet_user() function
+greet_user("Rikhi")
