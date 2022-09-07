0efd5e0630f87cd3662b212789581274bbac5020
Rikhi Ram Satnami <57804353+RamRikhi@users.noreply.github.com>
2022-09-07 21:26:15 +0530

    Feature/python basics (#21)
    * chapter-1
    
    * chapter-1-single line comment
    
    * multi line comments program
    
    * comment.py file is removed from main branch
    
    * python chapter 2 started
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * Chapter-1 is now completed
    
    * python learing is in progress
    
    * md file updated
    
    * chapter 2 started
    
    * calculator
    
    Co-authored-by: rikhi.ram <rikhi.ram@qovarian.com>

---
+++ 02-Chapter/calculator.py
@@ -0,0 +1,14 @@
+
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def division(a, b):
+    return a/b
+
+print("Addtion: " + str(add(10, 20)))
+print("Substraction: " + str(sub(10, 20)))
+print("Multiply: " + str(multiply(10, 20)))
+print("Division: " + str(division(10, 20)))
