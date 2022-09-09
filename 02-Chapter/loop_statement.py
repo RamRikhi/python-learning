b960d844c3007db3a342890d26b1717f9fa76981
rikhi.ram <rikhi.ram@qovarian.com>
2022-09-09 18:25:49 +0530

    conflix fixed for loop_statement.py

--- 02-Chapter/loop_statement.py
+++ 02-Chapter/loop_statement.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 
 # Author: Rikhi
 # Date: 09/09/2022
 # Copyright @ 2022
 
 #This fucntion is able to filter even numbers
-=======
->>>>>>> b8c6cf76b1f50f686acb1a4e3bb9adf2edcb9777
+
 def find_even_number(num):
     for index in num:
         if index % 2 == 0:
