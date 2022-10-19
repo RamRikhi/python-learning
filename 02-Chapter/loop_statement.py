872cb47311016cba912d163c431a92097eb020c4
rikhi.ram <rikhi.ram@qovarian.com>
2022-10-19 16:37:13 +0530

    updated function

--- 02-Chapter/loop_statement.py
+++ 02-Chapter/loop_statement.py
@@ -8,6 +8,6 @@
 def find_even_number(num):
     for index in num:
         if index % 2 == 0:
-            print("Even number: " + str(index))
+            print("Even number: " + str(index) * 2)
 
 find_even_number([10,20,2,3,4,5,9,10])
